df54803d4e6f9da6cecb4716d988368d2ccd9ca4
Roberto D'Ambrosio <roberto8601@gmail.com>
2022-02-02 15:27:23 +0100

    add header + jumbotron

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,19 @@
 @endsection
 
 @section('pageContent')
-    @foreach ($comic as $item)
-        <div class="card">
-            <img src="{{$item['thumb']}}" alt="comic-view">
-            <h4>{{$item['series']}}</h4>
-        </div>
-    @endforeach>
+<div class="hero">
+</div>
+ <div class="container">
+          <h2>CURRENT SERIES</h2>
+          <div class="comic-container">
+                @foreach ($comic as $item)
+                    <div class="card">
+                        <img src="{{$item['thumb']}}" alt="comic-view">
+                        <p>{{$item['series']}}</p>
+                    </div>
+                @endforeach
+                </div>
+          <button class="btn-content">Load more</button>
+      </div>
+
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,12 @@
 <header>
-    
+    <div class="container header">
+        <div class="logo">
+            <img src="{{asset('img/dc-logo.png')}}"" alt="dc-logo">
+        </div>
+        <nav>
+            <ul>
+                <li>Home</li>
+            </ul>
+        </nav>
+    </div>
 </header>
