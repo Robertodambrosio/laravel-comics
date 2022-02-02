f7da1aa15b1bf5ff2ccb74e94e6ae18797109a08
Roberto D'Ambrosio <roberto8601@gmail.com>
2022-02-02 15:51:41 +0100

    add footer (es completo)

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,91 @@
 <footer>
-    
+    <div class="footer-top-bg">
+        <div class="container">
+            <div class="option-container">
+                <div class="option">
+                    <img src="{{asset('img/buy-comics-digital-comics.png')}}" :alt="buycomic">
+                    <span>Digital comics</span>
+                </div>
+                <div class="option">
+                    <img src="{{asset('img/buy-comics-merchandise.png')}}" :alt="buycomic">
+                    <span>Dc merchandise</span>
+                </div>
+                <div class="option">
+                    <img src="{{asset('img/buy-comics-shop-locator.png')}}" :alt="buycomic">
+                    <span>Comic shop locator</span>
+                </div>
+                <div class="option">
+                    <img src="{{asset('img/buy-comics-subscriptions.png')}}" :alt="buycomic">
+                    <span>Subscribe</span>
+                </div>
+                <div class="option">
+                    <img src="{{asset('img/buy-dc-power-visa.svg')}}" :alt="buycomic">
+                    <span>dc power visa</span>
+                </div>
+            </div>
+        </div>
+    </div>
+        <div class="footer-main-bg">
+        <div class="container">
+            <div class="footer-menu-sx">
+                <div class="list-container">
+                    <ul>
+                        <h3>Title</h3>
+                        <li><a href="">Test</a></li>
+                        <li><a href="">Test</a></li>
+                        <li><a href="">Test</a></li>
+                        <li><a href="">Test</a></li>
+                        <li><a href="">Test</a></li>
+                        <li><a href="">Test</a></li>
+                        <li><a href="">Test</a></li>
+                    </ul>
+                    <ul>
+                        <h3>Title</h3>
+                        <li><a href="">Test</a></li>
+                        <li><a href="">Test</a></li>
+                    </ul>
+                    <ul>
+                        <h3>Title</h3>
+                        <li><a href="">Test</a></li>
+                        <li><a href="">Test</a></li>
+                        <li><a href="">Test</a></li>
+                        <li><a href="">Test</a></li>
+                        <li><a href="">Test</a></li>
+                        <li><a href="">Test</a></li>
+                        <li><a href="">Test</a></li>
+                        <li><a href="">Test</a></li>
+                        <li><a href="">Test</a></li>
+                        <li><a href="">Test</a></li>
+                        <li><a href="">Test</a></li>
+                    </ul>
+                    <ul>
+                        <h3>Title</h3>
+                        <li><a href="">Test</a></li>
+                        <li><a href="">Test</a></li>
+                        <li><a href="">Test</a></li>
+                        <li><a href="">Test</a></li>
+                        <li><a href="">Test</a></li>
+
+                    </ul>
+                </div>
+            </div>
+            <div class="footer-logo">
+                <img src="{{asset('img/dc-logo-bg.png')}}" alt="logo-footer">
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="footer-bottom-sx">
+                <button class="btn"><span>Sign-up now!</span></button>
+            </div>
+            <div class="footer-bottom-dx">
+                <img src="{{asset('img/footer-facebook.png')}}">
+                <img src="{{asset('img/footer-periscope.png')}}">
+                <img src="{{asset('img/footer-pinterest.png')}}">
+                <img src="{{asset('img/footer-twitter.png')}}">
+                <img src="{{asset('img/footer-youtube.png')}}">
+            </div>
+        </div>
+    </div>
 </footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,11 +1,20 @@
 <header>
     <div class="container header">
         <div class="logo">
-            <img src="{{asset('img/dc-logo.png')}}"" alt="dc-logo">
+            <img src="{{asset('img/dc-logo.png')}}" alt="dc-logo">
         </div>
         <nav>
             <ul>
-                <li>Home</li>
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Movies</li>
+                <li>Tv</li>
+                <li>Games</li>
+                <li>Collectibles</li>
+                <li>Videos</li>
+                <li>Fans</li>
+                <li>News</li>
+                <li>Shop</li>
             </ul>
         </nav>
     </div>
